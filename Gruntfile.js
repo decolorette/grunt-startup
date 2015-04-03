cda4cefceebc9a5a257b7d6f44a4af37c6b5a3ee
Ovi Farcas <ovidiu@jazio.net>
2015-04-03 23:53:14 +0200

    Configure grunt sass and uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,46 +1,62 @@
+'use strict';
+
 module.exports = function(grunt) {
 
   // Init configuration object.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
-    // Tasks.
+    // Describe tasks.
     /////////////////////////////////////////////////
+    watch: {
+      sass: {
+        // You can use html snippets.
+        files: ['**/*.{scss,sass}', 'assets/scss/**/*.html'],
+        // Tasks to be watched.
+        tasks: ['sass', 'uglify']
+      }
+    },
+
+
+    sass: {
+      dist: {
+        files: [{
+          // Dynamic mappings, allow the below options.
+          expand: true,
+          // Instead naming files individually, specify entire folders.
+          // Source.
+          cwd: 'assets/scss',
+          src: ['*.scss'],
+          // Destination.
+          dest: 'css',
+          ext: '.css'
+        }]
+      }
+    },
+
 
     // Minify the js files from src folder and move them to build folder.
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
+      // In case of many js files define the task dynamically
+      // see http://gruntjs.com/configuring-tasks#building-the-files-object-dynamically
       build: {
         src: 'assets/sjs/<%= pkg.name %>.js',
         dest: 'js/<%= pkg.name %>.min.js'
       }
     },
 
-    // Define sass configuration.
-    sass: {
-      dist: {
-        files: {
-          'css/style.css' : 'assets/sass/style.scss'
-        }
-      }
-    },
-
-    watch: {
-      css: {
-        files: '**/*.scss',
-        tasks: ['sass']
-      }
-    }
-
   });
-
-  // Load the plugin that provides the "uglify" task.
+  
+  // Load tasks plugins.
+  /////////////////////////////////////////////////
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  // Here set the running task.
+  /////////////////////////////////////////////////
+  grunt.registerTask('default', ['watch', 'uglify:build']);
 };
--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-##Startup project for a grunt powered javascript application.
+##Kickstart for a Grunt powered javascript application.
+
+### Goal
+This projects help you get started running Grunt with two main plugins allowing to compile Sass (grunt-contrib-sass) and to minify javascript (grunt-contrib-uglify).
 
 ###Prerequisites
-Install grunt
+Install grunt.
+
+You need node.js and its installer: npm. The best way to install node.js is via its installer: https://nodejs.org/. Then run ```sudo npm install npm -g```
+
+Now install grunt command line tool (command i or install):
 ```
 $ npm i -g grunt-cli
 /home/you/npm/bin/grunt -> /home/you/npm/lib/node_modules/grunt-cli/bin/grunt
@@ -11,29 +18,41 @@ grunt-cli@0.1.13 /home/jazio/npm/lib/node_modules/grunt-cli
 └── findup-sync@0.1.3 (lodash@2.4.1, glob@3.2.11)
 ```
 
+Then run ```npm init``` to generate package.json.
+
 You need to install needed grunt plugins:
 ```
-$ npm install grunt --save-dev
-$ npm install grunt-contrib-nodeunit --save-dev
-$ npm install grunt-contrib-uglify --save-dev
+$ npm i grunt --save-dev
+$ npm i grunt-contrib-sass --save-dev
+$ npm i grunt-contrib-uglify --save-dev
 ```
-and whatever.
+or whatever.
 
+Option --save-dev (or -S) will save it to to devDependencies. 
 These will save entries to package.json.
 
-Then run ```npm install```
 
-###Startup grunt
+
+###Run grunt
 ```$ grunt```
 
 will output:
 
 ```
+ - Waiting...
+>> File "assets/scss/style.scss" changed.
+Running "sass:dist" (sass) task
+
 Running "uglify:build" (uglify) task
 >> 1 file created.
-Done, without errors.
-```
 
+Done, without errors.
+
+```
+Now for every edit of scss file or javascript (sjs), grunt will generate and copy files in place.
 
 ###Notes on Configuration
-When manually edit the package.json there are two sections of dependencies: dependencies are required to run, devDependencies only to develop
+
+dependencies vs devDependencies.
+
+When manually edit the package.json there are two sections of dependencies: dependencies are required to run, devDependencies only to develop. 
